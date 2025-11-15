c22d8204a1c7f9d51940993add8269a85d75035f
yrwnds <l.zanini.nunes@gmail.com>
2025-11-14 22:43:32 -0300

    esqueleto de login

--- src/modules/root/root-component.ts
+++ src/modules/root/root-component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from '@angular/material/card';
 import {RouterOutlet} from '@angular/router';
+import {MatFormField} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root-component',
   imports: [
     MatCard,
     MatCardTitle,
+    MatFormField,
     MatCardSubtitle,
     MatCardHeader,
     MatCardContent,
